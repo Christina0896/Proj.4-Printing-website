abadf2c9d3735b1a948d4bf36bd81140e3e6688c
Cristina <cristinabandeira82@gmail.com>
2025-03-03 18:39:40 +0000

    add Topbar

---
+++ src/components/topbar.jsx
@@ -0,0 +1,9 @@
+function Topbar() {
+  return (
+    <div className="topbar flex justify-center items-center gap-8 ">
+      <h1 className="font-bold">Free shipping on all EU orders €50+</h1>
+      <button className="bg-white font-bold p-1 px-5">Shop Now</button>
+    </div>
+  );
+}
+export default Topbar;
