3183b7aec369ff56f863eba0c6af860d8f29e955
Cristina <cristinabandeira82@gmail.com>
2025-03-06 13:33:07 +0000

    add HeroSection and Slider components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { useState } from 'react';
 import Topbar from './components/topbar';
 import Navbar from './components/navbar';
+import HeroSection from './components/herosection';
 
 function App() {
   return (
     <div>
       <Topbar />
       <Navbar />
+      <HeroSection />
     </div>
   );
 }
---
+++ src/components/herosection.jsx
@@ -0,0 +1,51 @@
+import Slider from './slide';
+function HeroSection() {
+  return (
+    <div className="heroSection container mx-auto flex items-center justify-center ">
+      {/**/}
+      <div className="leftSlide p-4 ">
+        <h3 className="flex font-bold items-center justify-center mt-15">Create your own</h3>
+        <h2 className=" flex flex-col text-[68px] font-bold mt-5">
+          <span>Make the most</span>
+          <ul className=" typewriter ">
+            <li>
+              <span>of our t-shirt</span>
+            </li>
+            <li>
+              <span>favourite custom</span>
+            </li>
+            <li>
+              <span>designer ideas</span>
+            </li>
+          </ul>
+          <span>printing</span>
+        </h2>
+        <p className="text-[18px] max-w-[550px] mt-5 mb-2.5">
+          What’s more, we do it right! A full administration printing background. Print shirts for yourself or your
+          online business
+        </p>
+         {/*Button*/}
+        <div className="leftSlidebtn flex pb-15">
+          <button className="defaultbtn  font-bold">Shop Now </button>
+          <button className="flex ms-6  font-bold items-center">
+            How We Work <img className="ms-3" src="/src/assets/play.svg" alt="" />
+          </button>
+        </div>
+        <div className="numbers flex">
+          <h2 className="flex flex-col ">
+            12k+ <span>Collections</span>
+          </h2>
+          <div className="smallBorder mx-10"></div>
+          <h2 className=" flex flex-col">
+            26k+ <span>Items trusted to deliver</span>
+          </h2>
+        </div>
+      </div>
+       {/*Image Carousel*/}
+      <div className="rightSlide">
+        <Slider />
+      </div>
+    </div>
+  );
+}
+export default HeroSection;
