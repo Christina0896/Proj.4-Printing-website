d3457f1d57e27454613b4b29bdd297edd19bf14a
Cristina <cristinabandeira82@gmail.com>
2025-03-10 19:24:32 +0000

    add new images and CardSlide component; update Vite config and package.json for deployment

---
+++ src/components/cardSlide.jsx
@@ -0,0 +1,20 @@
+import TextSection from './textsection';
+import { useEffect } from 'react';
+import Glide from '@glidejs/glide';
+
+function CardSlide() {
+  return (
+    <div>
+      <div className="pt-20">
+        <TextSection
+          title="Our picks for you"
+          subtitle="Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, optio nostrum suscipit voluptas
+            explicabo. "
+        />
+      </div>
+      {/*<!-- Component: Carousel with controls inside --> */}
+    </div>
+  );
+}
+
+export default CardSlide;
